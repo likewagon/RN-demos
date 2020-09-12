49bb0603dbeb7b6d852805997c29eabff67f2266
maxus <maxus@maxuss-Mac.local>
2020-09-12 07:21:12 -0700

    u

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ import SideMenu from './src/sideMenu';
 import Chart from './src/chart';
 import NavigationRedux from './src/navigationRedux';
 import DateMaskModernPicker from './src/dateMaskModernPicker';
+import SnapCarousel from './src/snapCarousel';
 
 const App: () => React$Node = () => {
 
@@ -26,7 +27,8 @@ const App: () => React$Node = () => {
     SideMenu: <SideMenu />,
     Chart: <Chart />,
     NavigationRedux: <NavigationRedux />,
-    DateMaskModernPicker: <DateMaskModernPicker />
+    DateMaskModernPicker: <DateMaskModernPicker />,
+    SnapCarousel: <SnapCarousel />
   }
 
   const [demo, setDemo] = useState();
@@ -42,6 +44,7 @@ const App: () => React$Node = () => {
             <TouchableOpacity style={{alignSelf: 'center', marginTop: 20}} onPress={() => setDemo(demos.Chart)}><Text>Chart</Text></TouchableOpacity>
             <TouchableOpacity style={{alignSelf: 'center', marginTop: 20}} onPress={() => setDemo(demos.NavigationRedux)}><Text>Navigation Integrated with Redux Thunk</Text></TouchableOpacity>
             <TouchableOpacity style={{alignSelf: 'center', marginTop: 20}} onPress={() => setDemo(demos.DateMaskModernPicker)}><Text>TextInput Date Mask And Modern DateTimePicker</Text></TouchableOpacity>
+            <TouchableOpacity style={{alignSelf: 'center', marginTop: 20}} onPress={() => setDemo(demos.SnapCarousel)}><Text>Snap Carousel and Pagination</Text></TouchableOpacity>
           </ScrollView>
         </SafeAreaView>
         :
---
+++ src/snapCarousel/index.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+} from 'react-native';
+
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+
+export default class MyCarousel extends React.Component {
+  state = {
+    entries: [
+      {name: 'Shelbie'},
+      {name: 'Finnlay'},
+      {name: 'Kian'},
+      {name: '1Shelbie'},
+      {name: '1Finnlay'},
+      {name: '1Kian'},
+      {name: '2Shelbie'},
+      {name: '2Kian'},
+    ]
+  }
+
+  _renderItem({ item, index }) {
+    return (
+      <View style={{ width: 200, height: 200, marginTop: 100, borderWidth: 1 }}>
+        <Text>{item.name}</Text>
+      </View>
+    )
+  }
+
+  get pagination() {
+    const { entries, activeSlide} = this.state;
+    return (
+        <Pagination
+          dotsLength={entries.length}
+          activeDotIndex={activeSlide}
+          containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            marginHorizontal: 8,
+            backgroundColor: 'rgba(255, 255, 255, 0.92)'
+          }}
+          inactiveDotStyle={{
+            // Define styles for inactive dots here
+          }}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.6}
+        />
+    );
+  }
+
+  render() {
+    return (
+        <SafeAreaView style={{width: 300, height: 500}}>
+          <Carousel
+            data={this.state.entries}
+            sliderWidth={300}
+            itemWidth={200}
+            renderItem={this._renderItem}
+            onSnapToItem={(index) => this.setState({ activeSlide: index })}
+          />
+          {this.pagination}
+        </SafeAreaView>
+    );
+  }
+}
