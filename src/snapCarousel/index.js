f04a1f7387c53428dd07b034922a9541ae954a4c
maxus <maxus@maxuss-Mac.local>
2020-09-12 07:43:23 -0700

    u

--- src/snapCarousel/index.js
+++ src/snapCarousel/index.js
@@ -7,21 +7,21 @@ import {
 
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 
-export default class MyCarousel extends React.Component {
-  state = {
-    entries: [
-      {name: 'Shelbie'},
-      {name: 'Finnlay'},
-      {name: 'Kian'},
-      {name: '1Shelbie'},
-      {name: '1Finnlay'},
-      {name: '1Kian'},
-      {name: '2Shelbie'},
-      {name: '2Kian'},
-    ]
-  }
+export default function MyCarousel() {
+  const entries = [
+    { name: 'Shelbie' },
+    { name: 'Finnlay' },
+    { name: 'Kian' },
+    { name: '1Shelbie' },
+    { name: '1Finnlay' },
+    { name: '1Kian' },
+    { name: '2Shelbie' },
+    { name: '2Kian' },
+  ]
 
-  _renderItem({ item, index }) {
+  const [activeSlide, setActiveSlide] = React.useState(0);
+
+  function _renderItem({ item, index }) {
     return (
       <View style={{ width: 200, height: 200, marginTop: 100, borderWidth: 1 }}>
         <Text>{item.name}</Text>
@@ -29,41 +29,32 @@ export default class MyCarousel extends React.Component {
     )
   }
 
-  get pagination() {
-    const { entries, activeSlide} = this.state;
-    return (
-        <Pagination
-          dotsLength={entries.length}
-          activeDotIndex={activeSlide}
-          containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
-          dotStyle={{
-            width: 10,
-            height: 10,
-            borderRadius: 5,
-            marginHorizontal: 8,
-            backgroundColor: 'rgba(255, 255, 255, 0.92)'
-          }}
-          inactiveDotStyle={{
-            // Define styles for inactive dots here
-          }}
-          inactiveDotOpacity={0.4}
-          inactiveDotScale={0.6}
-        />
-    );
-  }
-
-  render() {
-    return (
-        <SafeAreaView style={{width: 300, height: 500}}>
-          <Carousel
-            data={this.state.entries}
-            sliderWidth={300}
-            itemWidth={200}
-            renderItem={this._renderItem}
-            onSnapToItem={(index) => this.setState({ activeSlide: index })}
-          />
-          {this.pagination}
-        </SafeAreaView>
-    );
-  }
+  return (
+    <SafeAreaView style={{ width: 300, height: 500 }}>
+      <Carousel
+        data={entries}
+        sliderWidth={300}
+        itemWidth={200}
+        renderItem={_renderItem}
+        onSnapToItem={(index) => setActiveSlide(index)}
+      />
+      <Pagination
+        dotsLength={entries.length}
+        activeDotIndex={activeSlide}
+        containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 8,
+          backgroundColor: 'rgba(255, 255, 255, 0.92)'
+        }}
+        inactiveDotStyle={{
+          // Define styles for inactive dots here
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+    </SafeAreaView>
+  );
 }
